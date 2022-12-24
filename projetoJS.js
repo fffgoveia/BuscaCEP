711c9994790e074f250867928df5ea9b99664c00
fffgoveia <115665368+fffgoveia@users.noreply.github.com>
2022-12-24 20:08:46 -0300

    Add files via upload

---
+++ projetoJS.js
@@ -0,0 +1,45 @@
+function criaElementoResultadoSucesso(value){
+    const result = document.querySelector("#result");
+    result.innerHTML="";
+    if (!!value.cep){
+        for(const property in value){
+            result.insertAdjacentHTML("beforeend",`<li>${property}: ${value[property]}</li>`);
+        }
+    }else {
+        criaElementoResultadoErro("CEP não encontrado!!!");
+    }
+}
+
+function criaElementoResultadoErro(value){
+    const result = document.querySelector("#result");
+    result.innerHTML = "";
+    result.insertAdjacentHTML(
+        "beforeend",
+        `<h2 style="color:#F00">${value}</h2>`
+    );
+}
+
+function pesquisaCEP(cep){
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((result) => {
+        criaElementoResultadoSucesso(result);
+    })
+    .catch((err) => {
+        criaElementoResultadoErro("CEP Inválido!!!");
+    });
+}
+
+const form = document.querySelector("form");
+const inputCEP = document.querySelector("#cep");
+
+form.addEventListener("submit", function (e){
+    e.preventDefault();
+    const cep = inputCEP.value.replace(/\D/g, "");
+    if (/^[0-9]{8}$/.test(cep)){
+        pesquisaCEP(cep);
+    }else{
+        criaElementoResultadoErro("CEP inválido!!!");
+    }
+});
